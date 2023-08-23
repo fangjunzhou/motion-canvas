14b535780945c863b665d6e38edd03c082bf5eba
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-08-23 04:04:51 -0500

    feat(ui): added editable property event field

--- packages/ui/src/components/sidebar/PropertyEvent.tsx
+++ packages/ui/src/components/sidebar/PropertyEvent.tsx
@@ -1,21 +1,44 @@
 import {Pane} from '../tabs';
 import {usePropertyInspection} from '../../contexts';
-import {AutoField} from '../fields';
-import {Group, Label, Separator} from '../controls';
+import {
+  isType,
+  MetaField,
+  NumberMetaField,
+  Vector2,
+  Vector2MetaField,
+} from '@motion-canvas/core';
+import {Separator} from '../controls';
+import {MetaFieldView} from '../meta';
 
 export function PropertiesEvent() {
-  const {event} = usePropertyInspection();
+  const {event, scene} = usePropertyInspection();
 
   return (
     <Pane title="Property Event Inspector" id="property-event-pane">
       <Separator size={1} />
       {event
-        ? Object.entries(event.property).map(([key, value]) => (
-            <Group>
-              <Label>{key}</Label>
-              <AutoField value={value} />
-            </Group>
-          ))
+        ? Object.entries(event.property).map(([key, value]) => {
+            let metaField = new MetaField(key, value);
+            if (isType(value)) {
+              switch (value.toSymbol()) {
+                case Vector2.symbol:
+                  metaField = new Vector2MetaField(key, value as Vector2);
+                  break;
+
+                default:
+                  break;
+              }
+            } else if (typeof value === 'number') {
+              metaField = new NumberMetaField(key, value);
+            }
+
+            metaField.onChanged.subscribe(val => {
+              event.property[key] = val;
+              scene.reload();
+            });
+
+            return <MetaFieldView field={metaField} />;
+          })
         : 'Click on a property event on the track to view its properties.'}
     </Pane>
   );
