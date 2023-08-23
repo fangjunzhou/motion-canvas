05fe52852910f2713add0d59f6c54e31520f17a8
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-08-23 15:58:54 -0500

    feat(examples): added property event examples

--- packages/template/src/scenes/example.tsx
+++ packages/template/src/scenes/example.tsx
@@ -1,5 +1,12 @@
 import {makeScene2D, Circle} from '@motion-canvas/2d';
-import {waitFor, waitUntil, createRef} from '@motion-canvas/core';
+import {
+  waitFor,
+  waitUntil,
+  createRef,
+  useProperty,
+  Vector2,
+  all,
+} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
   const circle = createRef<Circle>();
@@ -9,7 +16,18 @@ export default makeScene2D(function* (view) {
   );
 
   yield* waitUntil('circle');
-  yield* circle().scale(2, 2).to(1, 2);
+  const keyFrame1 = useProperty('key-frame-1', {
+    scale1: 200,
+    scale2: 100,
+    move1: new Vector2(500, 0),
+    move2: new Vector2(-500, 0),
+  });
+  yield* all(
+    circle()
+      .scale(keyFrame1.scale1 / 100, 2)
+      .to(keyFrame1.scale2 / 100, 2),
+    circle().position(keyFrame1.move1, 2).to(keyFrame1.move2, 2),
+  );
 
   yield* waitFor(5);
 });
