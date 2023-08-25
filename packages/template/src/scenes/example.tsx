c59a8fc25c8868319bd5f5eaeeddef1147ee2716
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-08-25 00:49:30 -0500

    feat(examples): added new property event demo

--- packages/template/src/scenes/example.tsx
+++ packages/template/src/scenes/example.tsx
@@ -1,11 +1,17 @@
-import {makeScene2D, Circle} from '@motion-canvas/2d';
+import {makeScene2D, Circle, Txt} from '@motion-canvas/2d';
 import {
-  waitFor,
   waitUntil,
   createRef,
   useProperty,
   Vector2,
   all,
+  ObjectMetaField,
+  NumberMetaField,
+  Vector2MetaField,
+  StringMetaField,
+  waitFor,
+  MetaField,
+  range,
 } from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
@@ -16,18 +22,61 @@ export default makeScene2D(function* (view) {
   );
 
   yield* waitUntil('circle');
-  const keyFrame1 = useProperty('key-frame-1', {
-    scale1: 200,
-    scale2: 100,
-    move1: new Vector2(500, 0),
-    move2: new Vector2(-500, 0),
-  });
-  yield* all(
-    circle()
-      .scale(keyFrame1.scale1 / 100, 2)
-      .to(keyFrame1.scale2 / 100, 2),
-    circle().position(keyFrame1.move1, 2).to(keyFrame1.move2, 2),
+  const keyFrame1 = useProperty(
+    new ObjectMetaField('key-frame-1', {
+      scale: new NumberMetaField('scale', 100),
+      move: new Vector2MetaField('move', new Vector2(500, 0)),
+      demoObject: new ObjectMetaField('demoObject', {
+        objNumber: new NumberMetaField('objNumber', 3.14),
+        objVector: new Vector2MetaField('objVector', new Vector2(3.14, 6.28)),
+        nestedObject: new ObjectMetaField('nestedObject', {
+          nestedNumber: new NumberMetaField('nestedNumber', 6.28),
+        }),
+      }),
+    }),
   );
 
-  yield* waitFor(5);
+  yield* all(
+    circle().scale(keyFrame1.scale.get() / 100, 2),
+    circle().position(keyFrame1.move.get(), 2),
+  );
+
+  const keyFrame2 = useProperty(
+    new ObjectMetaField('key-frame-2', {
+      scale: new NumberMetaField('scale', 100),
+      move: new Vector2MetaField('move', new Vector2(-500, 0)),
+    }),
+  );
+  const keyFrame2_2 = useProperty(
+    new ObjectMetaField('key-frame-2.5', {
+      scale2: new NumberMetaField('scale', 100),
+      move2: new Vector2MetaField('move', new Vector2(-500, 0)),
+    }),
+  );
+
+  yield* all(
+    circle().scale(keyFrame2.scale.get() / 100, 2),
+    circle().position(keyFrame2.move.get(), 2),
+  );
+
+  const keyFrame3 = useProperty(
+    new ObjectMetaField('key-frame-3', {
+      text: new StringMetaField('text', 'DEMO TEXT'),
+      fontSize: new NumberMetaField('fontSize', 100),
+    }),
+  );
+
+  const textRef = createRef<Txt>();
+  view.add(
+    <Txt
+      ref={textRef}
+      fill={'white'}
+      fontSize={keyFrame3.fontSize.get()}
+      text={keyFrame3.text.get()}
+    />,
+  );
+
+  yield* textRef().opacity(0).opacity(1, 1);
+
+  yield* waitFor(1);
 });
